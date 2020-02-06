c92564c56229a9b33752e33a2c25d45fff2ced59
dobertin <dbertinpro@gmail.com>
2020-02-06 16:39:03 +0100

    [UPDATE] index.js for link routes cakes

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 3000;
+const cakesRoutes = require('./routes/cakes');
 
+// Midelware
+app.use(express.json());
+
+// First route
 app.get('/', (req, res) => {
-    res.send('Welcome, to cake shop ;)');
+    res.send('Welcome, to cakes shop ;)');
 });
+
+app.use('/api/cakes', cakesRoutes);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port} for cake API`);
-
 })
